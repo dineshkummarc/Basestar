60165572c4d309227d71caa08d24033bc516be50
Senmiao Liu <zir.echo@gmail.com>
2010-08-28 09:04:53 +0800

    temp info

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var genji = require("./lib/genji/lib/genji");
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.end('Please check the <a href="http://nodeknockout.com/teams/fewtter">team page</a> for more info about this project!');
 });
 
 server.listen(80);
