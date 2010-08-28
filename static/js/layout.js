a2da7966825fd15779f9a0dc6f22ef7b69f717ca
Senmiao Liu <zir.echo@gmail.com>
2010-08-29 02:31:36 +0800

    console

--- static/js/layout.js
+++ static/js/layout.js
@@ -16,25 +16,28 @@
             }]
         }
     };
-    
+
+
+
     function _panel(id, label, options) {
         options = options || {};
         return {
             view: 'Box',
             rect: options.rect || '300 400',
-            anchors: options.anchors || 'top left right',
+            anchors: 'top left right bottom',
             childViews: [{   
                 view: 'Box',
                 background: 'theme(panel)',
-                rect: '300 24',
+                rect: options.labelRect || '300 24',
                 anchors: 'top left right',
                 childViews: [{
                     view: 'Label',
-                    rect: '50 0 200 24',
+                    rect: '0 0 200 24',
                     anchors: 'left top right bottom',
                     text: label,
                     style: {
                         textAlign: 'center',
+                        width: '100%',
                         fontWeight: 'bold'
                     }
                 }]
@@ -42,10 +45,11 @@
             {
                 id: id,
                 view: 'Box',
-                rect: '0 25 300 375',
+                rect: options.containerRect || '0 25 300 375',
                 anchors: 'top left right bottom',
                 style: {
-                    border: '1px solid #ccc'
+                    border: '1px solid #ccc',
+                    overflow: 'auto'
                 }
             }
             ]
@@ -61,7 +65,7 @@
                 view: 'Box',
                 rect: '1000 600',
                 anchors: 'left top right bottom',
-                //minSize: '800 0',
+                minSize: '800 0',
                 childViews: [
                 views.topToolbar,
                 {
@@ -78,21 +82,31 @@
                         anchors: 'left top right bottom',
                         handlePosition: 400,
                         vertical: true,
-                        topChildViews: [_panel('panelProject', 'Project browser')],
-                        bottomChildViews: [_panel('panelNavigator', 'Code Navigator')]
+                        topPane: _panel('panelProject', 'Project browser'),
+                        bottomPane: _panel('panelNavigator', 'Code Navigator')
                     }],
                     // code editor and info window like console, monitor etc.
                     rightChildViews: [{
-                        view: 'VSplitPane',
-                        rect: '0 0 700 600',
-                        anchors: 'left top right',
+                        view: 'Box',
+                        rect: '0 0 700 700',
+                        anchors: 'left top right bottom',
                         vertical: true,
-                        handlePosition: 500
+                        handlePosition: 600,
+                        childViews: [_panel('panelConsole', 'Console', 
+                            {rect:'0 600 300 200', labelRect: '698 24', containerRect: '1 25 698 120'})]
                     }]
                 }]
-            }).attachTo( window, '1000 600' , {
-                minSize: '800 0'
-            });
+            }).attachTo( window, '1000 600');
+            // popup for buttons
+            uki({id:'ppNewProject', view: 'Popup', rect: '0 20 240 120', anchors: 'right top', relativeTo: uki('#btnNewProject')[0]
+                , childViews: [
+                    {view: 'Label',    rect: '10 10 50 24', anchors: 'left top', html: 'Name:'},
+                    {id:'inputProjectName', view: 'TextField',rect: '60 10 170 24', anchors: 'left top', value: "", placeholder: 'of your project'},
+                    {view: 'Label',    rect: '10 50 50 24', anchors: 'left top', html: 'Url:'},
+                    {id:'inputProjectUrl', view: 'TextField',rect: '60 50 170 24', anchors: 'left top', value: "", placeholder: 'of your GIT repository'},
+                    {id:'btnGitClone', view: 'Button', rect: '160 86 70 24', anchors: 'right bottom', text: "Clone"},
+                ]
+            }).hide();
         },
 
         dialog: function (id, title) {
