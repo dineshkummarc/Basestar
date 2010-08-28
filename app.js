69b1aa028487aab3c646662f771fa84107ef5755
Senmiao Liu <zir.echo@gmail.com>
2010-08-28 10:57:08 +0800

    handle static files view renderer home page

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 // Main application module
 
-function hello(handler) {
-    handler.sendHTML("Hello world!");
+
+// Dependencies and settings
+var Path = require("path"),
+FileHandler = genji.web.handler.FileHandler,
+staticRoot = Path.join(__dirname, "/static"),
+viewRoot = Path.join(__dirname, "/view"),
+nun = require("nun");
+
+/* http endpoint */
+
+function index(handler) {
+    var ctx = {
+        staticUrl: genji.settings.staticUrl
+    }
+    _render("/index.xhtml", ctx, function(html) {
+        handler.sendHTML(html);
+    })
+}
+
+// serve static file under ./static
+function staticFile(handler, filepath) {
+    handler.setRoot(staticRoot);
+    handler.staticFile(filepath);
+}
+
+/* private functions */
+
+// render mustache template by nun
+function _render(path, context, callback) {
+    nun.render(Path.join(viewRoot, path), context, {compress: false, cache: true}, function(err, result) {
+       if (err) {
+            throw err;
+        }
+        var buffer = '';
+        result.addListener('data', function (c) {
+            buffer += c;
+        }).addListener('end', function () {
+            callback(buffer);
+        });
+    });
 }
 
 module.exports = [
-    ["^/hello/$", hello, "get"]
+    ["^/$", index, "get"],
+    [FileHandler, "^/static/(.*)$", staticFile, "get"]
 ];
