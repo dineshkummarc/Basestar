dce66c3984b772a5129210f16d473841a6fac9cd
Senmiao Liu <zir.echo@gmail.com>
2010-08-28 08:53:06 +0800

    test submodule

---
+++ server.js
@@ -0,0 +1,10 @@
+var genji = require("./lib/genji/lib/genji");
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Hello world\n");
+});
+
+server.listen(80);
